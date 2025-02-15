2f8b9bd020d1664be9897165c7af6d80a89941a5
Shamiksha <silkyraj23@gmail.com>
2025-02-15 23:17:02 +0530

    [shamiksha] add: testing parameterized tests code added✨

---
+++ Day05/src/main/java/com/capgemini/junit_problems/testing_parameterized_tests/NumberUtils.java
@@ -0,0 +1,8 @@
+package com.capgemini.junit_problems.testing_parameterized_tests;
+
+public class NumberUtils {
+    // Method to check if a number is even
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
